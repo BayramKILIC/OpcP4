3171757b9622721382cb2dfbf417ca94519ce6e3
BayramKILIC <bayi.kilic@yahoo.fr>
2019-07-18 11:25:01 +0300

    creation du controller TicketController et de la page d'acceuil twig

---
+++ src/Controller/TicketController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TicketController extends AbstractController
+{
+    /**
+ * @Route("/ticket", name="ticket")
+ */
+    public function index()
+    {
+        return $this->render('ticket/index.html.twig', [
+            'controller_name' => 'TicketController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('ticket/home.html.twig', [
+        ]);
+    }
+}
